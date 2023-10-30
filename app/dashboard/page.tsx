7b1dde5503d2e12379d977d32a83f7f724992589
Adeyinka Inioluwa John <boluwatifeeri@gmail.com>
2023-10-30 13:04:40 +0100

    FEAT: Completed the sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,21 @@
-import NavBar from '../components/NavBar/NavBar'
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import SideBar from '../components/SideBar/page';
 
 const DashboardPage = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
-      <NavBar />
+      <NavBar toggleSidebar={toggleSidebar} />
+      <SideBar isOpen={isSidebarOpen} />
     </div>
-  )
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
